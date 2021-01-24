6c2543007fea44791122d21854994246a7437a7f
IsaacGHoward <howardisaac100@gmail.com>
2021-01-24 14:43:58 -0800

    Implemented saveEdit

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {axiosWithAuth} from '../Util/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,7 +9,17 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  const getColors = () => {
+    axiosWithAuth().get('/colors')
+    .then(res => {
+      console.log(res);
+      setColorList(res.data); 
+    })
+    .catch(err => console.log(err));
+  }
+  useEffect(() => {
+    getColors();
+  }, [])
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
